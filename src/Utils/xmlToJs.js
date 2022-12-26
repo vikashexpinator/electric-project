5c4cf5be0e9554a430d824f3848700ab5572b8c7
vikashexpinator <110026331+vikashexpinator@users.noreply.github.com>
2022-12-26 18:12:52 +0530

    update live url to to connect backend

--- src/Utils/xmlToJs.js
+++ src/Utils/xmlToJs.js
@@ -7,7 +7,7 @@ export function xmlToJson(setfirst) {
   // Set format to used in axios request.
   var config = {
     method: "get",
-    url: "http://localhost:3005/",
+    url: "https://electricbackendserver.onrender.com/",
     headers: {},
   };
 
